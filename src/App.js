ab87a773aab2a6951383bc42971e9f8dbde44e41
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-08-07 22:17:14 -0300

    Upload project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles.css";
+import Header from './components/Header';
+import Team from './components/Team';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Team />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Team/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function Team() {
+    return (
+        <div className='title section text-center'>
+            <h2>Nosso <span class="color">Time</span></h2>
+            <div className='border'></div>
+        </div>
+    );
+}
+
+export default Team;
