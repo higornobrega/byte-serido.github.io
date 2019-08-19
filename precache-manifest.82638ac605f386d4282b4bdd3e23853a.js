d9dc6933e6f374b7edf9d3891eaca5a6d9e39760
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-08-19 18:32:07 -0300

    deploy

---
+++ precache-manifest.82638ac605f386d4282b4bdd3e23853a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee290de507d7a5c60cca18935e58aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebcd951cd9eacdf828be",
+    "url": "/static/css/main.8c08a09c.chunk.css"
+  },
+  {
+    "revision": "0fb46556f1466dbdff67",
+    "url": "/static/js/2.9e3ca644.chunk.js"
+  },
+  {
+    "revision": "ebcd951cd9eacdf828be",
+    "url": "/static/js/main.816bfbb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
