b92e2fc1e175dea2d46d9f6bce96508db5384a58
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-11-23 01:20:21 -0300

    Finalizando about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,10 +4,14 @@ import './styles.css';
 function About() {
     return (
         <div className='container'>
-            <div className='title'>
-            <h2>Sobre a <span class="color">Byte Seridó</span></h2>
-            
-            <div className='border'></div>
+            <div className='title-about'>
+                <h2>Sobre a <span class="color">Byte Seridó</span></h2>
+
+                <div className='border-about'>
+                    <p>
+                        Empresa de T.I da Universidade Federal do Rio Grande do Norte - CERES - Caicó/RN, nossa empresa júnior tem como objetivo proporcionar uma vivência em projetos, gestão e cultura empreendedora para seus integrantes. Nossa empresa é uma associação civil sem fins econômicos, formada e gerida por alunos da UFRN.
+                </p>
+                </div>
 
             </div>
         </div>
